d008b42675338c7aa1ccc32e2b137a835abbfc19
sen <pangs@shinetechchina.com>
2015-06-19 13:04:45 +0800

    change some page style.

--- FFCG.Loop.Web/FFCG.Loop.Web/script/app.js
+++ FFCG.Loop.Web/FFCG.Loop.Web/script/app.js
@@ -1,16 +1,17 @@
 
 angular.module("loop", ["ngRoute", "pascalprecht.translate", "loop.controllers"]);
 angular.module("loop.controllers", []);
+angular.module("loop.services", []);
 
 angular.module("loop").config(["$routeProvider", function ($routeProvider) {
     $routeProvider.when("/first", {
         controller: "first",
-        templateUrl: "first.html"
+        templateUrl: "views/first.html"
     }).when("/second", {
         controller: "second",
-        templateUrl: "second.html"
+        templateUrl: "views/second.html"
     }).otherwise({
-        redirectTo: "/"
+        redirectTo: "/first"
     });
 }]);
 angular.module("loop").config(["$translateProvider", function ($translateProvider) {
@@ -23,7 +24,52 @@ angular.module("loop").config(["$translateProvider", function ($translateProvide
 
 angular.module('loop.controllers').controller("first", ["$scope", function ($scope) {
 
-    $scope.status = ["Preliminary", "Applied", "Preliminary", "Planned", "Registered"];
+    $scope.status = ["Preliminary", "Applied", "Planned", "Registered"];
+    $scope.selectedStatus = [];
+    $scope.statusClick = function (statu) {
+        if ($($scope.selectedStatus).filter(function (index, n) { return n == statu; }).length) {
+            $scope.selectedStatus = $($scope.selectedStatus).filter(function (index, n) { return n != statu; });
+        } else {
+            $scope.selectedStatus.push(statu);
+        }
+        filterEvents();
+    };
+    $scope.isSelected = function (statu) {
+        return $($scope.selectedStatus).filter(function (index, n) { return n == statu; }).length;
+    };
+
+    function filterEvents(topic) {
+
+    }
+
+    $scope.manageTopic = function () {
+        $("#manageTopicModal").modal("show");
+    };
+    
+    $scope.allTopic = ["SOA", "BI", "ITIL", "Biztalk"];
+    $scope.isTopicSelected = function (topic) {
+        return $($scope.selectedTopic).filter(function (index, n) { return n == topic; }).length;
+    };
+    $scope.selectedTopic = ["SOA", "BI"];
+    $scope.topicClick = function (topic) {
+        if ($($scope.selectedTopic).filter(function (index, n) { return n == topic; }).length) {
+            $scope.selectedTopic = $($scope.selectedTopic).filter(function (index, n) { return n != topic; });
+        } else {
+            $scope.selectedTopic.push(topic);
+        }
+        filterEvents();
+    };
+    $scope.addCustomerTopic = function () {
+        $scope.alreadyExist = false;
+        if ($($scope.allTopic).filter(function (index, n) { return n == $scope.customerTopic; }).length) {
+            $scope.alreadyExist = true;
+        } else {
+            $scope.selectedTopic.push($scope.customerTopic);
+            $scope.allTopic.push($scope.customerTopic);
+            $scope.customerTopic = '';
+        }
+
+    };
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -31,7 +77,19 @@ angular.module('loop.controllers').controller("first", ["$scope", function ($sco
             right: 'month,agendaWeek,agendaDay'
         },
         eventClick: function (calEvent, jsEvent, view) {
-            console.log(calEvent, jsEvent, view);
+            $scope.selectedEvent = calEvent.data;
+            $scope.$digest();
+        },
+        eventMouseover: function (event, jsEvent, view) {
+            $(jsEvent.currentTarget).popover({
+                placement: "bottom",
+                content: function () {
+                    return "hello world.";
+                }
+            }).popover("show");
+        },
+        eventMouseout: function (event, jsEvent, view) {
+            $(jsEvent.currentTarget).popover("hide");
         },
         defaultDate: new Date(),
         editable: true,
@@ -40,7 +98,15 @@ angular.module('loop.controllers').controller("first", ["$scope", function ($sco
             {
                 title: 'SOA Masterclass',
                 start: '2015-06-11',
-                backgroundColor: "red"
+                backgroundColor: "red",
+                data: {
+                    title: "SOA Masterclass",
+                    date: "2015-06-11",
+                    time: "09:00-12:00",
+                    place: "shan xi xian",
+                    status: "Registration",
+                    topics: ["SOA", "Biztalk EDI", "Microsoft"]
+                }
             },
             {
                 title: 'Agile Webinar',
@@ -95,6 +161,7 @@ angular.module('loop.controllers').controller("first", ["$scope", function ($sco
 //            }
         ]
     });
+
 }]);
 
 
@@ -103,6 +170,16 @@ angular.module('loop.controllers').controller("mainController", ["$scope", funct
 }]);
 angular.module('loop.controllers').controller("second", ["$scope", function ($scope) {
     console.log("second-controller");
+}]);
+
+angular.module("loop.services").factory("eventService", ["", function () {
+    var eventService = {};
+    eventService.getData = function () {
+        return [];
+    };
+
+
+    return eventService;
 }]);
 
 
